d3a283078d4e878f8d98524c27cacb3385e31b84
Nathan Briscoe <N.Briscoe1@gmail.com>
2016-04-05 13:27:27 -0500

    set up Hello World

--- client/client.js
+++ client/client.js
@@ -1,13 +1,30 @@
 /**
  * Created by NathanBriscoe on 3/28/16.
  */
-var app = angular.module('WhatToEat', []);
+var app = angular.module('WhatToEat', ['ngRoute']);
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/index.html', {
+            templateUrl: 'views/index.html',
+            controller: 'homeController'
+        })
+        .when('/recipes', {
+            templateUrl: 'views/recipes.html',
+            controller: 'recipeListsController'
+        });
+
+    $locationProvider.html5Mode(true);
+
+    }]);
+
+    app.controller('homeController', ['$scope', function ($scope) {
+
+    }]);
+
+    app.controller('recipeListsController', ['$scope', function ($scope) {
+
+    }]);
 
 
-app.controller('recipeListsController', function($scope){
-    $scope.recipes = [
-        {'name':
-
-    }]
-});
 
