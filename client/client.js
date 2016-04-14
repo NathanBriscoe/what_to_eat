ee169f09eef669148995f0d2362a9eb0fd4b336b
Nathan Briscoe <N.Briscoe1@gmail.com>
2016-04-14 16:06:12 -0500

    server side API call plus npm request installed

--- client/client.js
+++ client/client.js
@@ -22,73 +22,58 @@ app.config(['$routeProvider','$locationProvider', function($routeProvider, $loca
 
     }]);
                     //Controller for the home...?
-    app.controller('homeController', ['$scope', '$location', function ($scope, $location) {
-        $scope.recipes = function(){
-            $location.path('/recipes')
-        };
-        $scope.buildRecipes = function(){
-            $location.path('/buildrecipes')
-        };
-        $scope.today = moment().format('MMMM Do YYYY');
+app.controller('homeController', ['$scope', '$location', function ($scope, $location) {
+    $scope.recipes = function(){
+        $location.path('/recipes')
+    };
+    $scope.buildRecipes = function(){
+        $location.path('/buildrecipes')
+    };
+    $scope.today = moment().format('MMMM Do YYYY');
 
-        var myTimer = setInterval(myClock, 1000);
-        function myClock() {
-            document.getElementById("demo").innerHTML =
-                new Date().toLocaleTimeString();
+    var myTimer = setInterval(myClock, 1000);
+    function myClock() {
+        document.getElementById("demo").innerHTML =
+            new Date().toLocaleTimeString();
+    }
+}]);
+                //Controller for the recipes List
+app.controller('recipeListsController', ['$scope', '$http', function ($scope, $http) {
+
+}]);
+            //Controller for building recipes
+app.controller('buildRecipesController', ['$scope', '$location', function ($scope, $location){
+    //$scope.addIngredient = function
+
+}]);
+
+app.controller('searchByIngredientsController', ['$scope', '$http', function($scope, $http){
+
+    $scope.ingredientTypedIn = [];
+
+    $scope.addNewIngredient = function(){
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.ingredientTypedIn.indexOf($scope.addMe) == -1) {
+            $scope.ingredientTypedIn.push($scope.addMe);
+        } else {
+            $scope.errortext = "The ingredient is already entered.";
         }
-    }]);
-                    //Controller for the recipes List
-    app.controller('recipeListsController', ['$scope', '$http', function ($scope, $http) {
+        $scope.addMe = "";
+    };
 
-    }]);
-                //Controller for building recipes
-    app.controller('buildRecipesController', ['$scope', '$location', function ($scope, $location){
-        //$scope.addIngredient = function
+    $scope.removeItem = function(ingredients){
+        $scope.errortext = "";
+        $scope.ingredientTypedIn.splice(ingredients, 1);
 
-    }]);
+    };
 
-    app.controller('searchByIngredientsController', ['$scope', '$http', function($scope, $http){
+    $scope.submitIngredients = function(){
+        $http.get("/whatCanIMake", $scope.ingredientTypedIn).then(function(response){
+            $scope.list = response.data;
+            $scope.recipes = response;
+            $scope.ingredientTypedIn = [];
+        });
+    };
 
-        $scope.ingredientTypedIn = [];
-        //$scope.data = [];
-
-        $scope.addNewIngredient = function(){
-            $scope.errortext = "";
-            if (!$scope.addMe) {return;}
-            if ($scope.ingredientTypedIn.indexOf($scope.addMe) == -1) {
-                $scope.ingredientTypedIn.push($scope.addMe);
-            } else {
-                $scope.errortext = "The ingredient is already entered.";
-            }
-            $scope.addMe = "";
-        };
-
-        $scope.removeItem = function(ingredients){
-            $scope.errortext = "";
-            $scope.ingredientTypedIn.splice(ingredients, 1);
-
-        };
-
-//[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
-
-
-
-
-
-        //This is a bunch of nonsense below. I'm testing
-
-        $scope.submitIngredients = function(ingredientTypedIn){
-            console.log("We are going to go look for ");
-            console.log(ingredientTypedIn);
-            $http.get("http://www.recipepuppy.com/api/?i=" + ingredientTypedIn + "").then(function(response){
-                $scope.list = response.data;
-                console.log(ingredientTypedIn);
-                $scope.recipes = response;
-                $scope.ingredientTypedIn = [];
-                //$scope.data = [];
-                //$scope.data.push(response.data);
-                //$scope.ingredientTypedIn.push(response.data);
-            });
-        };
-
-    }]);
+}]);
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,22 +5,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var router = express.Router();// get an instance of the express Router
+var request = require('request');
 
-// ROUTES FOR OUR API
-// =============================================================================
+//[][][][][][][][][][][][][][][][][][][]Start Making API server side call here[][][][][][][][][][][][][][][][][][][][][]
 
-// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
-router.get('/', function(request, response) {
-    response.json({ message: 'hooray! welcome to our api!' });
+router.get("/whatCanIMake", function(request, response){
+
+    var inputIngredients = request.body.params;
+    var api = "http://www.recipepuppy.com/api/?i=" + inputIngredients + "";
+
+    request(api, function(err, response){
+
+    });
+
+
 });
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
+//[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
 
 router.get('/', function(request, response){
     var joinedPath = path.join(__dirname, '../public/views/index.html');
